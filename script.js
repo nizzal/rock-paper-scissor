5a07c12baf5dc82b1a40d5ed5322c1b1f2681c65
Nizzal Gurung <nizzal.gurung20@gmail.com>
2021-01-20 12:16:39 +0545

    added css and some js

--- script.js
+++ script.js
@@ -1,49 +1,82 @@
-function game() {
-    for (let i = 1; i <= 5; i++) {
-        function playRound(playerSelection, computerSelection) {
-            if ( playerSelection == "rock" && computerSelection == "rock") {
-                // console.log("Draw!")
-                return "Draw"
-            } else if (playerSelection == "rock" && computerSelection == "paper") {
-                // console.log("You Lose! Paper beats Rock")
-                return "You Lose! Paper beats Rock"
-            } else if (playerSelection == "rock" && computerSelection == "scissor") {
-                // console.log("You Win! Paper beats Rock")
-                return "You Win! Paper beats Rock";
-            } 
-        }
-        function computerPlay() {
-            var items = ['rock', 'paper', 'scissor']
-            return items[Math.floor(Math.random()*items.length)];
-        }
+let bt1 = document.getElementById('rock');
+let bt2 = document.getElementById('paper');
+let bt3 = document.getElementById('scissor');
+let res = document.getElementById('result');
+let cs = document.getElementById('c-score');
+let ps = document.getElementById('p-score');
+
+pScore = 0;
+cScore = 0;
+
+var comp = document.getElementById('c-select');
+var pl = document.getElementById('p-select');
+
+
+bt1.onclick = function() {
+    playerSelection = selection[0];
+    play();
+};
+
+bt2.onclick = function() {
+    playerSelection = selection[1];
+    play();
+};
+
+
+let playerSelection;
+
+bt3.addEventListener('click', function() {
+    playerSelection = selection[2];
+    play();
+})
+
+var selection = ["rock", "paper", "scissor"];
+
+
+function play() {
+    let computerSelection;
+    computerSelection = computerPlay();
     
-        // const playerSelection = prompt("Enter your plays!");
-        const playerSelection = "rock";
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log( "Player chose: " +playerSelection, "and Computer chose: " + computerSelection)
-        console.log("----------------------------------------------------------")
-        playRound();
+
+    function computerPlay() {
+        var items = ['rock', 'paper', 'scissor']
+        return items[Math.floor(Math.random()*items.length)];
+    };
+    function playRound(playerSelection, computerSelection) {
+        if ( playerSelection === "rock" && computerSelection === "rock") {
+            res.innerText = "Draw!";
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            res.innerText = "You Lose! Paper beats Rock";
+            cs.innerText = cScore ++;
+        } else if (playerSelection === "rock" && computerSelection === "scissor") {
+            res.innerText = "You Win! Paper beats Rock";
+            ps.innerText = pScore ++;
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            res.innerText = "You Lose! Paper beats Rock";
+            ps.innerText = pScore ++;
+        } else if (playerSelection === "paper" && computerSelection === "paper") {
+            res.innerText = "Draw";
+        }else if (playerSelection === "paper" && computerSelection === "scissor") {
+            res.innerText = "Scissor beats paper";
+            cs.innerText = cScore ++;
+        } else if (playerSelection === "scissor" && computerSelection === "rock") {
+            res.innerText = "Rock beats scissor";
+            cs.innerText = cScore ++;
+        } else if (playerSelection === "scissor" && computerSelection === "paper") {
+            res.innerText = "Scissor beats paper";
+            ps.innerText = pScore ++;
+        } else if (playerSelection === "scissor" && computerSelection === "scissor") {
+            res.innerText = "Draw";
+        }
+        comp.innerText = computerSelection;
+        pl.innerText = playerSelection;
+        if (pScore > 5 && cScore < 5) {
+            alert("Hurray you won the match!");
+            location.reload();
+        } else if (cScore > 5 && pScore < 5){
+            alert("Try again next time!");
+            location.reload();
+        }
     }
-    
-
-    
+    playRound(playerSelection, computerSelection)
 }
-
-
-game();
-
-
-
-// function playRound(playerSelection, computerSelection) {
-//     if ( playerSelection == "rock" && computerSelection == "rock") {
-//         // console.log("Draw!")
-//         return "Draw"
-//     } else if (playerSelection == "rock" && computerSelection == "paper") {
-//         // console.log("You Lose! Paper beats Rock")
-//         return "You Lose! Paper beats Rock"
-//     } else if (playerSelection == "rock" && computerSelection == "scissor") {
-//         // console.log("You Win! Paper beats Rock")
-//         return "You Win! Paper beats Rock";
-//     } 
-// }
